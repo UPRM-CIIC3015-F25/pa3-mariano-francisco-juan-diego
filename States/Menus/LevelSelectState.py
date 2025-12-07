1e60a8dd3fb164c115ae8900158e57d44e2d5e9a
Bloquecitos <francisco.vidal2@upr.edu>
2025-12-06 21:43:24 -0400

    Filled the boss_abilities TODO (Task 9.1) Completed the conditionals related to Bosses in TODO (Task 9.2)

--- States/Menus/LevelSelectState.py
+++ States/Menus/LevelSelectState.py
@@ -91,7 +91,14 @@ class LevelSelectState(State):
                 #   Finally, make sure to reset the player’s round score to 0 at the end of this setup.
                 #   Avoid unnecessary repetition—use clear condition structure to make the logic readable.
                 self.playerInfo.roundScore = 0
-                
+                boss = lm.curSubLevel.bossLevel # Searches for next boss
+                if boss == "The Needle":
+                    self.playerInfo.amountOfHands = 1
+                elif boss == "The Water":
+                    self.playerInfo.amountOfDiscards = 0
+                elif boss == "The Manacle":
+                    self.playerInfo.amountOfHands -= 1
+                PlayerInfo.roundScore = 0
                 # Set target score for the new sublevel
                 self.playerInfo.score = self.playerInfo.levelManager.curSubLevel.score
                 
@@ -115,8 +122,9 @@ class LevelSelectState(State):
         #   Each key should be the name of a boss (e.g., "The Mark", "The Needle", etc.), and each value should describe
         #   what unique restriction or ability that boss applies during the round.
         #   This dictionary will later be used to look up and apply special effects based on which boss is active.
-        boss_abilities = {
-
+        boss_abilities = {"The Mark": "All Face cards are drawn face down", "The Needle": "Play only 1 hand",
+        "The House": "First hand is drawn face down", "The Hook": "Discards 2 random cards held in hand after every played hand",
+        "The Water": "Start with 0 discards", "The Manacle": "-1 hand size"
         }
 
         # Dict of boss with their color schemes
